5a941067c33b3d0514c568d93e2730f7ea03e02a
Ondrej Dusek <o.dusek@hw.ac.uk>
2018-05-24 16:35:10 +0000

    Adding get version retrieval

--- utils/log.py
+++ utils/log.py
@@ -1,6 +1,7 @@
 
 import logging
-
+import subprocess
+import datetime
 
 LOG_LEVELS = {'critical': logging.CRITICAL,
               'error': logging.ERROR,
@@ -8,3 +9,9 @@ LOG_LEVELS = {'critical': logging.CRITICAL,
               'info': logging.INFO,
               'debug': logging.DEBUG}
 
+
+def get_short_git_version(path='.'):
+    data = subprocess.check_output(['git', '-C', path, 'log', '--pretty=%h-%at', '-1']).strip()
+    shorthash, ts = data.split('-')
+    date = datetime.datetime.fromtimestamp(int(ts)).strftime('%Y-%m-%d-%H%M')
+    return '%s-%s' % (shorthash, date)
