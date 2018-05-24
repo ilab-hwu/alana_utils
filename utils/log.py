baa7f2f287be76c8bbf0dddf550fbed9797d43a6
Ondrej Dusek <o.dusek@hw.ac.uk>
2018-05-24 15:26:50 +0000

    Logging levels

---
+++ utils/log.py
@@ -0,0 +1,10 @@
+
+import logging
+
+
+LOG_LEVELS = {'critical': logging.CRITICAL,
+              'error': logging.ERROR,
+              'warning': logging.WARNING,
+              'info': logging.INFO,
+              'debug': logging.DEBUG}
+
